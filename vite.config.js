91cc01f24f5b53a5902b979b3be746a35ddf150a
Dubno Petro <petro.dubno@gmail.com>
2024-10-26 16:35:55 +0300

    Make PWA app

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,60 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: 'Minesweeper',
-})
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        short_name: "MineSweeper",
+        name: "MineSweeper",
+        icons: [
+          {
+            src: "favicon.ico",
+            sizes: "48x48",
+            type: "image/x-icon"
+          },
+          {
+            src: "logo192.png",
+            type: "image/png",
+            sizes: "192x192"
+          },
+          {
+            src: "logo512.png",
+            type: "image/png",
+            sizes: "512x512"
+          }
+        ],
+        start_url: ".",
+        display: "standalone",
+        theme_color: "#000000",
+        background_color: "#ffffff"
+      },
+      registerType: 'autoUpdate', // Automatically update the service worker
+      workbox: {
+        // Options for Workbox
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) => request.destination === 'document',
+            handler: 'NetworkFirst', // Use the NetworkFirst strategy for HTML documents
+          },
+          {
+            urlPattern: ({ request }) => request.destination === 'image',
+            handler: 'CacheFirst', // Use CacheFirst for images
+            options: {
+              cacheName: 'images', // Cache name
+              expiration: {
+                maxEntries: 50, // Limit the number of entries
+                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+              },
+            },
+          },
+          // Add more caching strategies as needed
+        ],
+      },
+    }),
+  ],
+  base: 'Minesweeper', // Set to your repository name
+});
